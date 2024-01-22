527ec03251b1d84b979bf670e628ccbe7e3fd05f
Zaizhou <1206315053@qq.com>
2024-01-23 02:32:17 +0800

    seperate the gamestate and game

--- othelloBase.py
+++ othelloBase.py
@@ -4,12 +4,87 @@ Implementation of a simple but hard game: Othello.
 https://zh.wikipedia.org/wiki/%E9%BB%91%E7%99%BD%E6%A3%8B
 '''
 
+from util import *
 from copy import deepcopy
+import numpy as np
+import copy
 
 class Othello():
     # Constants
     SIZE = 8
     def __init__(self):
+        self.gamestate = GameState()
+        self._boardHistory = [deepcopy(self.gamestate.board)]
+        self._turnHistory = [self.gamestate.turn]
+
+    def printBoard(self):
+        '''
+        Return a string representation of the board.
+        '''
+        print(
+            '\n'.join(["  0 1 2 3 4 5 6 7"]+[str(i)+" "+(' '.join(map(lambda x: 'X' if x == 1 else 'O' if x == -1 else '.', self.gamestate.board[i]))) for i in range(len(self.gamestate.board))])
+        )
+    
+    def place(self, x, y):
+        '''
+        Place a piece on the board.
+        '''
+        self.gamestate = self.gamestate.generateSuccessor(x,y)
+        self._boardHistory.append(deepcopy(self.gamestate.board))
+        self._turnHistory.append(self.gamestate.turn)
+    
+    def undo(self):
+        '''
+        Undo the move.
+        '''
+        self._boardHistory.pop()
+        self._turnHistory.pop()
+        
+        self.gamestate.board = deepcopy(self._boardHistory[-1])
+        self.gamestate.turn = self._turnHistory[-1]
+        
+
+    def isEnd(self):
+        '''
+        Check if the game is over.
+        '''
+        return self.gamestate.getBlackScore() + self.gamestate.getWhiteScore() == 64
+        
+    def getWinner(self):
+        '''
+        Return the winner.
+        '''
+        blackCount = self.gamestate.getBlackScore()
+        whiteCount = self.gamestate.getWhiteScore()
+
+        if whiteCount > blackCount:
+            return -1
+        elif whiteCount < blackCount:
+            return 1
+        else:
+            return 0
+    
+    def getScore(self):
+        '''
+        Return the score, 
+        currently for black X.
+        '''
+        return self.gamestate.getBlackScore() - self.gamestate.getWhiteScore()
+    
+    def getBoard(self):
+        '''
+        Return the board.
+        '''
+        return self.gamestate.board
+    
+    def getTurn(self):
+        '''
+        Return the current player.
+        '''
+        return self.gamestate.turn
+
+class GameState:
+    def __init__(self, board = None, turn = None):
         '''
         A 8*8 board, 1 for X / black, -1 for O/ white, 0 for empty.
 
@@ -28,36 +103,30 @@ class Othello():
 
         X / Black goes first.
         '''
-        self.board = [[0 for _ in range(8)] for _ in range(8)]
-        self.board[3][3] = self.board[4][4] = -1
-        self.board[3][4] = self.board[4][3] = 1
-        self.turn = 1
-        self._boardHistory = [deepcopy(self.board)]
-        self._turnHistory = [self.turn]
-        self._flipCount = 0
-        self._checkCount = 0
+        self.board = board
+        self.turn = turn
+        if board is None:
+            self.board = [[0 for _ in range(8)] for _ in range(8)]
+            self.board[3][3] = self.board[4][4] = -1
+            self.board[3][4] = self.board[4][3] = 1
+        if turn is None:
+            self.turn = 1
 
-    def printBoard(self):
-        '''
-        Return a string representation of the board.
-        '''
-        print(
-            '\n'.join(["  0 1 2 3 4 5 6 7"]+[str(i)+" "+(' '.join(map(lambda x: 'X' if x == 1 else 'O' if x == -1 else '.', self.board[i]))) for i in range(len(self.board))])
-        )
-    
-    def getValidPositions(self, turn):
+    def reverseTurn(self):
+        self.turn *= -1
+
+    def getValidPositions(self):
         '''
         Return a list of valid positions / moves for the current player.
         '''
-        
         validPositions = []
         for i in range(8):
             for j in range(8):
-                if self.board[i][j] == 0 and self._checkValid(i, j, turn):
+                if self.board[i][j] == 0 and self._checkValid(i, j):
                     validPositions.append((i, j))
         return validPositions
-    
-    def _checkValid(self, x, y, turn):
+
+    def _checkValid(self, x, y):
         '''
         Check if a move is valid for the current player.
         '''
@@ -65,168 +134,107 @@ class Othello():
             return False
         for dx in range(-1, 2):  # -1, 0, 1
             for dy in range(-1, 2):  # -1, 0, 1
-                self._checkCount = 0
                 if dx == dy == 0: # therefore it covers all 8 directions
                     continue
-                if self._checkDirection(x, y, dx, dy, turn):
+                if self._isValidDirection(x, y, dx, dy):
                     return True
         return False
-    
-    def _checkDirection(self, x, y, dx, dy, turn):
+
+    def _isValidDirection(self, x, y, dx, dy):
         '''
         Check if a move is valid for the current player in a specific direction.
         '''
-        if x + dx < 0 or x + dx >= 8 or y + dy < 0 or y + dy >= 8:
+        i, j = x + dx, y + dy
+        if i < 0 or i >= 8 or j < 0 or j >= 8 or self.board[i][j] != -self.turn:
             return False
-        if self.board[x + dx][y + dy] == -turn:
-            self._checkCount += 1
-            return self._checkDirection(x + dx, y + dy, dx, dy, turn)
-        elif self.board[x + dx][y + dy] == turn:
-            if self._checkCount != 0:
+        while 0 <= i < 8 and 0 <= j < 8:
+            if self.board[i][j] == self.turn:
                 return True
-        else:
-            return False
+            elif self.board[i][j] == -self.turn:
+                i, j = i + dx, j + dy
+            else:
+                 return False
+        return False
     
-    def place(self, x, y, turn):
+    def generateSuccessors(self):
         '''
-        Place a piece on the board.
+        Return a list of valid successors.
         '''
-        self.board[x][y] = turn
-        flips = self._flip(x, y, turn)
-        self.turn = -turn
-        # self._updateCount()
-        validPositions = self.getValidPositions(self.turn)
-        if len(validPositions) == 0:
-            self.turn = -self.turn # 变回来，因为这个时候下一个玩家没有合法的位置，所以当前玩家继续落子
-            validPositions = self.getValidPositions(self.turn)
-        self._boardHistory.append(deepcopy(self.board))
-        self._turnHistory.append(self.turn)
+        successors = []
+        validPositions =self.getValidPositions()
+        for x, y in validPositions:
+            successors.append(self.generateSuccessor(x, y))
+        return successors
 
-        return flips
+    def generateSuccessor(self, x, y):
+        '''
+        return a gamestate after placing stone in (x,y)
+        '''
+        successor = copy.copy(self)
+        successor.board[x][y] = self.turn
+        successor._flip(x, y)
+        successor.reverseTurn()
+        if successor.getValidPositions() == []:
+            successor.reverseTurn()
+
+        return successor
     
-    def _flip(self, x, y, turn):
+    def countFlips(self, x, y):
         '''
-        Flip pieces on the board.
+        Return the number of stones that would be flipped if a stone were placed at (x, y).
         '''
-        self._flipCount = 0
+        flipCount = 0
         for dx in range(-1, 2):
             for dy in range(-1, 2):
                 if dx == dy == 0:
                     continue
-                if self._checkDirection(x, y, dx, dy, turn):
-                    self._flipDirection(x, y, dx, dy, turn)
-        return self._flipCount
+                i, j = x + dx, y + dy
+                while 0 <= i < 8 and 0 <= j < 8 and self.board[i][j] == -self.turn:
+                    flipCount += 1
+                    i, j = i + dx, j + dy
+        return flipCount
 
-    def _flipDirection(self, x, y, dx, dy, turn):
+    def _flip(self, x, y):
         '''
-        Flip pieces on the board in a specific direction.
+       Flip pieces on the board if put stone on (x,y).
         '''
-        if x + dx < 0 or x + dx >= 8 or y + dy < 0 or y + dy >= 8:
-            return
-        if self.board[x + dx][y + dy] == -turn:
-            self.board[x + dx][y + dy] = turn
-            self._flipCount += 1
-            self._flipDirection(x + dx, y + dy, dx, dy, turn)
-        elif self.board[x + dx][y + dy] == turn:
-            return
-        else:
-            self.board[x + dx][y + dy] = turn
+        for dx in range(-1, 2):
+            for dy in range(-1, 2):
+                if dx == dy == 0:
+                    continue
+                if self._isValidDirection(x, y, dx, dy):
+                    i, j = x + dx, y + dy
+                    while 0 <= i < 8 and 0 <= j < 8 and self.board[i][j] == -self.turn:
+                        self.board[i][j] = self.turn
+                        i, j = i + dx, j + dy
 
-    def undo(self):
-        '''
-        Undo the move.
-        '''
-        self._flipCount = 0
-        self._boardHistory.pop()
-        self._turnHistory.pop()
-        
-        self.board = deepcopy(self._boardHistory[-1])
-        self.turn = self._turnHistory[-1]
-        
 
-    def isEnd(self):
-        '''
-        Check if the game is over.
-        '''
-        if self.getValidPositions(1) == [] and self.getValidPositions(-1) == []:
-            return True
-        else:
-            return False
-        
-    def getWinner(self):
-        '''
-        Return the winner.
-        '''
-        blackCount = 0
-        whiteCount = 0
+    def getBlackScore(self):
+        BlackCount = 0
         for i in range(8):
             for j in range(8):
                 if self.board[i][j] == 1:
-                    blackCount += 1
-                elif self.board[i][j] == -1:
-                    whiteCount += 1
-        
-        if whiteCount > blackCount:
-            return -1
-        elif whiteCount < blackCount:
-            return 1
-        else:
-            return 0
-    
-    def getScore(self):
-        '''
-        Return the score, 
-        currently for black X.
-        '''
-        blackCount = 0
+                    BlackCount += 1
+        return BlackCount
+
+    def getWhiteScore(self):
         whiteCount = 0
         for i in range(8):
             for j in range(8):
-                if self.board[i][j] == 1:
-                    blackCount += 1
-                elif self.board[i][j] == -1:
+                if self.board[i][j] == -1:
                     whiteCount += 1
-        
-        return blackCount - whiteCount
-    
-    def getBoard(self):
-        '''
-        Return the board.
-        '''
-        return self.board
-    
-    def getTurn(self):
-        '''
-        Return the current player.
-        '''
-        return self.turn
-    
-    def getState(self):
-        """返回当前棋盘的状态
+        return whiteCount
 
-        Returns:
-            64维向量
+class Agent:
+    """
+    An agent must define a getAction method
+    """
+    def __init__(self, index=0):
+        self.index = index
+
+    def getAction(self, state: GameState):
         """
-        return np.array(self.board, dtype=np.int).flatten()     # 返回一个一维数组
-    
-    def Add(self, my_color, pos):
-        """加入一个新的棋子
-
-        Arguments:
-            my_color {int} -- 1表示黑棋，-1表示白棋
-            pos {int} -- 位置
+        The Agent will receive a GameState (from either {pacman, capture, sonar}.py) and
+        must return an action from Directions.{North, South, East, West, Stop}
         """
-        if pos != 64:
-            (x, y) = (pos // self.board_size, pos % self.board_size)
-            self.board[x][y] = my_color
-            if my_color == 1:
-                self.black_chess.add((x, y))
-                self.board[x][y] = 1
-            elif my_color == -1:
-                self.white_chess.add((x, y))
-                self.board[x][y] = -1
-
-            if my_color == 1:
-                self.Reverse((x, y), self.black_chess, self.white_chess, my_color)
-            elif my_color == -1:
-                self.Reverse((x, y), self.white_chess, self.black_chess, my_color)
+        raiseNotDefined()
