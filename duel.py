4c3aa2a2045a51ecb5df7b442aa5d7b56345b726
Zaizhou <1206315053@qq.com>
2024-01-23 14:21:18 +0800

    dev: add cmd argument -r

--- duel.py
+++ duel.py
@@ -16,12 +16,8 @@ parser.add_argument('-w', '--white', type=str, default='greedy', choices=agents,
                     help='the white player')
 parser.add_argument('-s', '--speed', type=float, default=0, help='The speed of playing')
 parser.add_argument('-v', '--visual', action='store_true', help='Whether to visualize')
+parser.add_argument('-r', '--repeat', type=int, default=1, help='Number of games to play')
 
-args = parser.parse_args()
-
-# Create the game and canvas
-game = Othello()
-canvas = Canvas() if args.visual else None
 
 # Select the agent based on the command-line argument
 def get_agent(agent_type, model_path=None):
@@ -39,27 +35,43 @@ def get_agent(agent_type, model_path=None):
     else:
         raise ValueError(f"Unknown agent type: {agent_type}")
 
-agent_b = get_agent(args.black, 'RL_method/model/model_X.pth')
-agent_w = get_agent(args.white, 'RL_method/model/model_O.pth')
 
-# Main game loop
-while not game.isEnd():
-    state = game.gamestate
+def play_game(args):
+    # Main game loop
+    while not game.isEnd():
+        state = game.gamestate
 
-    canvas.draw_board(state.board, state.getValidPositions()) if args.visual else None
+        canvas.draw_board(state.board, state.getValidPositions()) if args.visual else None
 
-    # control the playing speed
-    time.sleep(args.speed)
-    if state.turn == 1:
-        x, y = agent_b.getAction(state)
-    else:
-        x, y = agent_w.getAction(state)
+        # control the playing speed
+        time.sleep(args.speed)
+        if state.turn == 1:
+            x, y = agent_b.getAction(state)
+        else:
+            x, y = agent_w.getAction(state)
 
-    game.place(x, y)
+        game.place(x, y)
 
-winner = "Black" if game.getWinner() == 1 else "White" if game.getWinner() == -1 else "No one (it's a tie)"
-print(f"Game Over. {winner} wins!")
+    winner = "Black" if game.getWinner() == 1 else "White" if game.getWinner() == -1 else "No one (it's a tie)"
+    print(f"Game Over. {winner} wins!")
+    return winner
 
-if args.visual:
-    canvas.draw_board(game.getBoard(), game.gamestate.getValidPositions())
-    canvas.game_over(game.getWinner())
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    # Create the game and canvas
+    game = Othello()
+    canvas = Canvas() if args.visual else None
+    # initialize agents 
+    agent_b = get_agent(args.black, 'RL_method/model/model_X.pth')
+    agent_w = get_agent(args.white, 'RL_method/model/model_O.pth')
+    # run game repeatedly
+    wins = 0
+    for _ in range(args.repeat):
+        wins += 1 if play_game(args) else None
+
+    win_rate = wins / args.repeat
+    print(f'Win rate for black player: {win_rate * 100}%')
+    if args.visual:
+        canvas.draw_board(game.getBoard(), game.gamestate.getValidPositions())
+        canvas.game_over(game.getWinner())
